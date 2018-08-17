a9c7a0521bcd854c376d47949f148680b31a209d
Luc Leysen <luc56.leysen@gmail.com>
2018-08-17 16:21:50 +0200

    Create project and nuget packages loaded

---
+++ ConsoleApp.SQLite/ConsoleApp.SQLite/Model.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp.SQLite
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.ConsoleApp.NewDb;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()
+                .Property(b => b.Url)
+                .IsRequired();
+        }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+
+        public Blog Blog { get; set; }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+}
