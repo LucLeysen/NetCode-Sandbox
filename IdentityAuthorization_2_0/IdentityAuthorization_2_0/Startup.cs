f4f57bc9fb82d438476e6e558e9575abf40bcd1c
Luc Leysen <luc56.leysen@gmail.com>
2018-08-22 16:15:45 +0200

    cleanup code

--- IdentityAuthorization_2_0/IdentityAuthorization_2_0/Startup.cs
+++ IdentityAuthorization_2_0/IdentityAuthorization_2_0/Startup.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
