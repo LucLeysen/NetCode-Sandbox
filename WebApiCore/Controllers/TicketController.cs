7fb2245e5652da48f8fd7efb906ee62cf2ab9efe
Luc Leysen <luc56.leysen@gmail.com>
2018-08-31 17:27:28 +0200

    Create ticket

---
+++ WebApiCore/Controllers/TicketController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly TicketContext _context;
+
+        public TicketController(TicketContext context)
+        {
+            _context = context;
+
+            if (!_context.TicketItems.Any())
+            {
+                _context.TicketItems.Add(new TicketItem {Concert = "Beyonce"});
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<TicketItem> GetAll()
+        {
+            return _context.TicketItems.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTicket")]
+        public IActionResult GetById(long id)
+        {
+            var ticket = _context.TicketItems.FirstOrDefault(t => t.Id == id);
+
+            if (ticket == null) return NotFound();
+
+            return new ObjectResult(ticket);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]TicketItem ticket)
+        {
+            if (ticket == null) return BadRequest();
+
+            _context.TicketItems.Add(ticket);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTicket", new { id = ticket.Id}, ticket);
+        }
+    }
+}
