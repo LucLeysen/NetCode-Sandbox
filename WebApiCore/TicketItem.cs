a8b31dc07b047720949cd78b0f502607ef073e54
Luc Leysen <luc56.leysen@gmail.com>
2018-08-31 16:09:31 +0200

    Add TicketItem

---
+++ WebApiCore/TicketItem.cs
@@ -0,0 +1,11 @@
+namespace WebApiCore
+{
+    public class TicketItem
+    {
+        public long Id { get; set; }
+        public string Concert { get; set; }
+        public string Artist { get; set; }
+        public bool Available { get; set; }
+
+    }
+}
