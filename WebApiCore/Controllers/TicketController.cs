7dc60d991b74849edb37afeb89851d1f8a6a1361
Luc Leysen <luc56.leysen@gmail.com>
2018-09-05 16:10:25 +0200

    Add CRUD and Swagger documentation

--- WebApiCore/Controllers/TicketController.cs
+++ WebApiCore/Controllers/TicketController.cs
@@ -39,14 +39,44 @@ namespace WebApiCore.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create([FromBody]TicketItem ticket)
+        public IActionResult Create([FromBody] TicketItem ticket)
         {
             if (ticket == null) return BadRequest();
 
             _context.TicketItems.Add(ticket);
             _context.SaveChanges();
 
-            return CreatedAtRoute("GetTicket", new { id = ticket.Id}, ticket);
+            return CreatedAtRoute("GetTicket", new {id = ticket.Id}, ticket);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] TicketItem ticket)
+        {
+            if (ticket == null || id != ticket.Id) return BadRequest();
+
+            var existingTicket = _context.TicketItems.FirstOrDefault(t => t.Id == id);
+
+            if (existingTicket == null) return NotFound();
+
+            existingTicket.Concert = ticket.Concert;
+            existingTicket.Artist = ticket.Artist;
+            existingTicket.Available = ticket.Available;
+            _context.TicketItems.Update(existingTicket);
+            _context.SaveChanges();
+
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var ticket = _context.TicketItems.FirstOrDefault(t => t.Id == id);
+            if (ticket == null) return NotFound();
+
+            _context.TicketItems.Remove(ticket);
+            _context.SaveChanges();
+
+            return new NoContentResult();
         }
     }
 }
--- WebApiCore/Startup.cs
+++ WebApiCore/Startup.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebApiCore
 {
@@ -27,6 +21,16 @@ namespace WebApiCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Info
+                {
+                    Title = "Cocert API",
+                    Version = "v1"
+                });
+            });
+
             services.AddDbContext<TicketContext>(opt => opt.UseInMemoryDatabase("TicketList"));
         }
 
@@ -34,13 +38,14 @@ namespace WebApiCore
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
             else
-            {
                 app.UseHsts();
-            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                s => { s.SwaggerEndpoint("/swagger/v1/swagger.json", "Ticket APi v1"); }
+            );
 
             app.UseHttpsRedirection();
             app.UseMvc();
