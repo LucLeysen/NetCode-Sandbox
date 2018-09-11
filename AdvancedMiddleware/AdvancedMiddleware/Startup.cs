6d2f054df105a30a41387163fae7f827cd682952
Luc Leysen <luc56.leysen@gmail.com>
2018-09-11 15:58:23 +0200

    create + wrapping middlezware

---
+++ AdvancedMiddleware/AdvancedMiddleware/Startup.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AdvancedMiddleware
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration
+        {
+            get;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(LogLevel.Information);
+            var logger = loggerFactory.CreateLogger("Midleware demo");
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.Use(async (context, next) =>
+            {
+                var timer = Stopwatch.StartNew();
+
+                logger.LogInformation($"==> beginning request in {env.ApplicationName}");
+                await next();
+
+                logger.LogInformation($" ==> completed request {timer.ElapsedMilliseconds} ms");
+            });
+            app.UseStaticFiles();
+
+            app.UseMvc();
+        }
+    }
+}
